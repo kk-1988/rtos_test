9bb0e241cbbd764fd3d7aa2c56521f1d0a3e2830
kk-1988 <xiaobo.kong@aliyun.com>
2020-09-08 07:29:46 +0800

    add file of portmacro.h

---
+++ 新建FreeRTOS工程/freertos/include/portmacro.h
@@ -0,0 +1,16 @@
+#ifndef PORTMACRO_H
+#define PORTMACRO_H
+
+#include "stdint.h"
+#include "stddef.h"
+
+/* 数据类型重定义 */
+#define portCHAR	char
+#define portFLOAT	float
+#define portDOUBLE	double
+#define portLONG	long
+#define portSHORT	short
+#define portSTACK_TYPE	uint32_t
+#define	portBASE_TYPE	long
+
+#endif
