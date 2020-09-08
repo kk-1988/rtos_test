f32e17ac5cb3ce63153c685419c10a7117cb64aa
kk-1988 <xiaobo.kong@aliyun.com>
2020-09-09 07:20:21 +0800

    modified the file of portmacro.h

--- 新建FreeRTOS工程/freertos/include/portmacro.h
+++ 新建FreeRTOS工程/freertos/include/portmacro.h
@@ -12,5 +12,17 @@
 #define portSHORT	short
 #define portSTACK_TYPE	uint32_t
 #define	portBASE_TYPE	long
+	
+typedef portSTACK_TYPE StackType_t£»
+typedef long BaseType_t
+typedef unsigned long UBaseType_t;
+
+#if( configUSE_16_BIT_TICKS == 1)
+	typedef uint16_t TickType_t;
+	#define portMAX_DELAY( TickType_t ) 0xffff
+#else
+	typedef uint32_t TickType_t;
+	#define portMAX_DELAY( TickType_t )0xffffffffUL
+#endif
 
 #endif
