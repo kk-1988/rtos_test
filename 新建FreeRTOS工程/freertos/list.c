138474d96f894903fc2374e415366bdeafb5b727
kk-1988 <xiaobo.kong@aliyun.com>
2020-09-14 08:56:44 +0800

    实现链表初始化

--- 新建FreeRTOS工程/freertos/list.c
+++ 新建FreeRTOS工程/freertos/list.c
@@ -1,5 +1,13 @@
 #include "list.h"
 
+/* 链表根节点初始化 */
+void VListInitialise(ListItem_t * const pxList)
+{
+	/* 将链表索引指针指向最后一个节点 */
+	pxList->pxIndex = (ListItem_t *)&(pxList->xListEnd);
+	
+}
+
 /* 节点初始化 */
 void vListInitialiseItem(ListItem_t * const pxItem)
 {
