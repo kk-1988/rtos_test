132f3942f147530faf6346db0bcdad9bc40fd624
kk-1988 <xiaobo.kong@aliyun.com>
2020-09-16 08:34:31 +0800

    实现链表初始化

--- 新建FreeRTOS工程/freertos/list.c
+++ 新建FreeRTOS工程/freertos/list.c
@@ -1,10 +1,12 @@
 #include "list.h"
 
 /* 链表根节点初始化 */
-void VListInitialise(ListItem_t * const pxList)
+void VListInitialise(List_t * const pxList)
 {
 	/* 将链表索引指针指向最后一个节点 */
 	pxList->pxIndex = (ListItem_t *)&(pxList->xListEnd);
+	/* 将链表最后一个节点的辅助排序的值设置为最大 */
+	pxList->xListEnd.xItemValue = portMAX_DELAY;
 	
 }
 
