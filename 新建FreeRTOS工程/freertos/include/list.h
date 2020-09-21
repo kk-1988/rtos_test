ea7382f10b5baef65a5636a579b9ca27fff7e33b
xiaobo.kong <xiaobo.kong@broadlink.com.cn>
2020-09-21 09:12:48 +0800

    修改list.h头文件

--- 新建FreeRTOS工程/freertos/include/list.h
+++ 新建FreeRTOS工程/freertos/include/list.h
@@ -3,7 +3,7 @@
 /* 节点结构体定义 */
 struct xLIST_ITEM
 {
-	TickType_t xItemValue;	/* 辅助值,用于帮助节点做顺序排列 */
+	TickType_t xItemValue;	/* 辅助值,用于帮助节点做顺序排列(升序排列) */
 	struct xLIST_ITEM *pxNext;	/* 指向链表下一个节点 */
 	struct xLIST_ITEM *pxPrevious;	/* 指向链表前一个节点 */
 	void *pvOwner;					/* 指向拥有该节点的内核对象，通常是TCB */
