b283cdcd8b4ac6b27221bf8f900dc69f23b679a7
xiaobo.kong <xiaobo.kong@broadlink.com.cn>
2020-09-29 08:57:57 +0800

    增加相关宏

--- 新建FreeRTOS工程/freertos/include/list.h
+++ 新建FreeRTOS工程/freertos/include/list.h
@@ -37,7 +37,21 @@ typedef struct xLSIT
 #define listGET_NEXT( pxListItem ) ( (pxListItem )->pxNext )
 
 /* 获取链表的最后一个节点 */
-#define 
+#define listGET_END_MARKER( pxList ) ( (ListItem_t const *) (& ( ( pxList )->xListEnd ) 0 ))
+
+/* 获取链表的节点数 */
+#define listCURRENT_LIST_LENGTH( pxList ) (( pxList )->uxNumberOfItems )
+
+/* 获取链表节点的OWNER，即TCB */
+#define listGET_OWNER_OF_NEXT_ENTRY( pxTCB, pxList )
+{
+	List_t * const pxConstList = ( pxList );
+	/* 节点索引指向链表第一个节点调整节点索引指针，指向下一个节点，
+		如果当前链表有N个节点，当第N次调用该函数时，pxIndex则指向第N个节点
+	 */
+	( pxConstList )->pxIndex = ( pxConstList )->pxIndex->pxNext;
+	/* 当前链表为空 */
+}
 
 void vListInitialise(List_t * const pxList);
 void vListInitialiseItem(ListItem_t * const pxItem);
