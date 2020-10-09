e6dc807d2f8005ba75c0f16d3b12f47cf4bf7f84
xiaobo.kong <xiaobo.kong@broadlink.com.cn>
2020-10-09 09:13:54 +0800

    添加任务切换demo代码

--- 新建FreeRTOS工程/User/main.c
+++ 新建FreeRTOS工程/User/main.c
@@ -1,27 +1,58 @@
 #include "list.h"
 
+/*
+* 全局
+*/
+portCHAR flag1;
+portCHAR flag2;
+
+//定义两个任务的任务栈(task1,task2)
+//TaskHandle_t Task1_Handle;
+#define TASK1_STACK_SIZE	20
+StackType_t Task1Stack[TASK1_STACK_SIZE];
+//TCB_t Task1TCB;		//任务控制块1
+
+//TaskHandle_t Task2_Handle;
+#define TASK2_STACK_SIZE	20
+StackType_t Task2Stack[TASK2_STACK_SIZE];
+//TCB_t Task2TCB;		//任务控制块2
+
+//定义一个任务函数
+void Task1_Entry(void *p_arg);
+void Task2_Entry(void *p_arg);
+
+
 int main(void)
 {
-	/* 根节点链表 */
-	List_t list;	
-	ListItem_t node1,node2,node3;
-	
-	/* 链表初始化 */
-	vListInitialise(&list);
-	/* 节点初始化 */
-	vListInitialiseItem(&node1);
-	vListInitialiseItem(&node2);
-	vListInitialiseItem(&node3);
-	
-	/* 插入 */
-	vListInsert(&list, &node1);
-	vListInsert(&list, &node2);
-	vListInsert(&list, &node3);
-	
+		
 	for(;;)
 	{
 		/* 啥事都不干 */	
 	}
 	
 	return 0;
+}
+
+//延时函数
+void delay(uint32_t count)
+{
+	for(; count!=0;count--);	
+}
+
+//定义一个任务函数
+void Task1_Entry(void *p_arg)
+{
+	while(1)
+	{
+		//todo
+	}
+}
+
+//定义一个任务函数
+void Task2_Entry(void *p_arg)
+{
+	while(1)
+	{
+		//todo
+	}
 }
