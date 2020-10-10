0a5887eac1bc50325724352779ca138ff5e9b4dd
xiaobo.kong <xiaobo.kong@broadlink.com.cn>
2020-10-10 09:00:06 +0800

    添加相关宏定义到FreeRTOSConfig.h的头文件中

--- 新建FreeRTOS工程/freertos/include/FreeRTOSConfig.h
+++ 新建FreeRTOS工程/freertos/include/FreeRTOSConfig.h
@@ -2,5 +2,15 @@
 #define FREERTOS_CONFIG_H
 
 #define configUSE_16_BIT_TICKS	0
+#define configMAX_TASK_NAME_LEN	( 16 )
+#define configSUPPORT_STATIC_ALLOCATION	1
+#define configMAX_PRIORITIES	( 5 )
 
-#endif
+#define configKERNEL_INTERRUPT_PRIORITY	255		/* 高四位有效，即等于0xff,或者是15 */
+#define configMAX_SYSCALL_INTERRUPT_PRIORITY	191	/* 高四位有效，即等于0xb0,或者是11 */
+
+#define xPortPendSVHandler	PendSV_Handler
+#define xPortSysTickHandler	SysTick_Handler
+#define xPortSVHandler		SVC_Handler
+
+#endif	/* FREERTOS_CONFIG_H */
