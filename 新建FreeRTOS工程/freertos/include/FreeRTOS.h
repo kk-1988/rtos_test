ad875c39be1cfa4ab2a0b44ee279326bcc22b360
xiaobo.kong <xiaobo.kong@broadlink.com.cn>
2020-10-10 08:54:32 +0800

    添加任务控制块结构体到FreeRTOS.h的头文件中

---
+++ 新建FreeRTOS工程/freertos/include/FreeRTOS.h
@@ -0,0 +1,19 @@
+#ifndef INC_FREERTOS_H
+#define INC_FREERTOS_H
+
+#include "FreeRTOSConfig.h"
+#include "projdefs.h"
+#include "protable.h"
+#include "list.h"
+
+typedef struct tskTaskControlBlock
+{
+	volatile StackType_t *pxTopOfStack;	/* 栈顶 */
+	ListItem_t		xStateListItem;		/* 任务节点 */
+	StackType_t		*pxStack;			/* 任务栈起始地址 */
+	char 			pcTaskName[ configMAX_TASK_NAME_LEN ];
+	
+}tskTCB;
+typedef tskTCB TCB_t;
+
+#endif		/* INC_FREERTOS_H */
