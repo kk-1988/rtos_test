cecb9cc98d15a420684f3fb17f15bc6c42280f7f
xiaobo.kong <xiaobo.kong@broadlink.com.cn>
2020-10-12 09:03:43 +0800

    增加task控制块变量申请

--- 新建FreeRTOS工程/User/main.c
+++ 新建FreeRTOS工程/User/main.c
@@ -11,14 +11,16 @@ portCHAR flag2;
 //TaskHandle_t Task1_Handle;
 #define TASK1_STACK_SIZE	20
 StackType_t Task1Stack[TASK1_STACK_SIZE];
-//TCB_t Task1TCB;		//任务控制块1(包括任务的栈指针，任务名称，任务的形参等)
 
 //TaskHandle_t Task2_Handle;
 #define TASK2_STACK_SIZE	20
 StackType_t Task2Stack[TASK2_STACK_SIZE];
-//TCB_t Task2TCB;		//任务控制块2
+
+TCB_t Task1TCB;		//任务控制块1(包括任务的栈指针，任务名称，任务的形参等)
+TCB_t Task2TCB;		//任务控制块2
 
 //定义一个任务函数
+void delay (uint32_t count);
 void Task1_Entry(void *p_arg);
 void Task2_Entry(void *p_arg);
 
