90f6966e8607b539456923d02bf9c43134f534dd
xiaobo.kong <xiaobo.kong@broadlink.com.cn>
2020-10-13 09:02:10 +0800

    添加任务创建接口函数xTaskCreateStatic

---
+++ 新建FreeRTOS工程/freertos/task.c
@@ -0,0 +1,27 @@
+
+
+
+#if( configSUPPORT_STATIC_ALLOCATION == 1 )
+	
+TaskHandle_t xTaskCreateStatic( TaskFunction_t pxTaskCode,		/* 任务入口 */
+							const char * const pcName,			/* 任务名称，字符串形式 */
+							const uint32_t ulStackDepth,		/* 任务栈大小，单位为字 */
+							void * const pvParameters,			/* 任务形参 */
+							StackType_t * const puxStackBuffer,	/* 任务栈起始地址 */
+							TCB_t * const pxTaskBuffer )		/* 任务控制块指针 */
+{
+	TCB_t	*pxNewTCB;
+	TaskHandle_t xReturn;
+	
+	if( ( pxTaskBuffer != NULL ) && ( puxStackBuffer != NULL ))
+	{
+		pxNewTCB = ( TCB_t * )pxTaskBuffer;
+		pxNewTCB->pxStack = ( StackType_t * ) puxStackBuffer;
+		
+		/* 创建新的任务 */
+		prvInitialiseNewTask( pxTaskCode,		/* 任务入口 */
+							)
+		
+	}
+	
+}							
