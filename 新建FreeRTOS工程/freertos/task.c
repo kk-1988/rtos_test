2e638e1ca422e98288222ecb6fa7d2267177a02d
xiaobo.kong <xiaobo.kong@broadlink.com.cn>
2020-10-21 08:53:17 +0800

    完善节点初始化接口

--- 新建FreeRTOS工程/freertos/task.c
+++ 新建FreeRTOS工程/freertos/task.c
@@ -57,5 +57,20 @@ static void prvInitialiseNewTask(	TaskFunction_t pxTaskCode,	/* 任务入口 */
 	
 	/* 获取栈顶指针 */
 	pxTopOfStack = pxNewTCB->pxStack + ( ulStackDepth - ( uint32_t ) 1 );
+	/* 向下做8字节对齐 */
+	pxTopOfStack = ( StackType_t * )(((uint32_t) pxTopOfStack ) & (~((uint32_t) 0x0007 )));
+	
+	/* 将任务的名称存储在TCB中 */
+	for( x= (UBaseType_t )0; x < (uBaseType_t ) configMAX_TASK_NAME_LEN; x++)
+	{
+		pxNewTCB->pcTaskName[ x ] = pcName[ x ];
+		
+		if( pcName[ x ] = 0x00 )
+		{
+			break;
+		}
+	}
+	
+	/* 任务名字的长度不能超过configMAX_TASK_NAME_LEN */
 	
 }
