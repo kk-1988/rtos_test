300e247d6770bec7afc8344bfb945bd9b58d4fd5
xiaobo.kong <xiaobo.kong@broadlink.com.cn>
2020-10-22 09:05:16 +0800

    完善port.c接口p

--- 新建FreeRTOS工程/freertos/portable/RVDS/ARM_CM3/port.c
+++ 新建FreeRTOS工程/freertos/portable/RVDS/ARM_CM3/port.c
@@ -15,6 +15,17 @@ StackType_t *pxPortInitialiseStack( StackType_t *pxTopOfStack, TaskFunction_t px
 {
 	/* 异常发生时，自动加载到CPU寄存器的内容 */
 	pxTopOfStack--;
+	*pxTopOfStack = portINITIAL_XPSR;	/* xPSR的bit24必须置1 */
+	pxTopOfStack--;
+	*pxTopOfStack = ( ( StackType_t ) pxCode ) & portSTART_ADDRESS_MASK;	/* PC,即任务入口函数 */
+	pxTopOfStack--;
+	*pxTopOfStack = ( StackType_t )prvTaskExitError;	/* LR,函数返回地址 */
+	pxTopOfStack -= 5;	/* R12,R3,R2 and R1 默认初始化为0 */
+	*pxTopOfStack = ( StackType_t )pvParameters;		/* R0,任务形参 */
 	
+	/* 异常发生时，手动加载到CPU寄存器的内容 */
+	pxTopOfStack -= 8;	/* R11,R10,R9,R8,R7,R6,R5 and R4默认初始化为0 */
 	
+	/* 返回栈顶指针，此时pxTopOfStack指向空闲栈 */
+	return pxTopOfStack;
 }
