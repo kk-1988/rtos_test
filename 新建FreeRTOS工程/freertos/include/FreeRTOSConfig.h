b20a0ca4b16e63e0111b880718182430f15f424f
xiaobo.kong <xiaobo.kong@broadlink.com.cn>
2020-12-07 09:00:53 +0800

    指定空闲任务最小栈大小

--- 新建FreeRTOS工程/freertos/include/FreeRTOSConfig.h
+++ 新建FreeRTOS工程/freertos/include/FreeRTOSConfig.h
@@ -6,8 +6,10 @@
 #define configSUPPORT_STATIC_ALLOCATION	1
 #define configMAX_PRIORITIES	( 5 )
 
-#define configKERNEL_INTERRUPT_PRIORITY	255		/* 高四位有效，即等于0xff,或者是15 */
-#define configMAX_SYSCALL_INTERRUPT_PRIORITY	191	/* 高四位有效，即等于0xb0,或者是11 */
+#define configKERNEL_INTERRUPT_PRIORITY		(255)		/* 高四位有效，即等于0xff,或者是15 */
+#define configMAX_SYSCALL_INTERRUPT_PRIORITY	(191)	/* 高四位有效，即等于0xb0,或者是11 */
+#define configMINIMAL_STACK_SIZE	( ( unsigned short )128 )	/* 空闲任务最小的栈大小 */
+
 
 #define xPortPendSVHandler	PendSV_Handler
 #define xPortSysTickHandler	SysTick_Handler
