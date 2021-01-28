851f6d34702edf97c7bd2fa9d0ed7b928ec96421
xiaobo.kong <xiaobo.kong@broadlink.com.cn>
2021-01-28 08:59:46 +0800

    多优先级任务接口相关补充

--- 新建FreeRTOS工程/freertos/include/FreeRTOS.h
+++ 新建FreeRTOS工程/freertos/include/FreeRTOS.h
@@ -9,9 +9,9 @@
 typedef struct tskTaskControlBlock
 {
 	volatile StackType_t *pxTopOfStack;	/* 栈顶 */
-	ListItem_t		xStateListItem;		/* 任务节点 */
-	StackType_t		*pxStack;			/* 任务栈起始地址 */
-	char 			pcTaskName[ configMAX_TASK_NAME_LEN ];
+	ListItem_t			xStateListItem;		/* 任务节点 */
+	StackType_t			*pxStack;			/* 任务栈起始地址 */
+	char 						pcTaskName[ configMAX_TASK_NAME_LEN ];
 	TickType_t      xTicksToDelay;		/* 用于延时 */
 	UBaseType_t 		uxPriority;				/* 任务优先级控制属性 */
 }tskTCB;
